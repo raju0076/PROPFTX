788bd3384fb75d16bac0e47a94672a8f443ef75a
raju <gnraju0076@gmail.com>
2025-05-29 13:17:21 +0530

    ll

--- client/propftx/src/App.jsx
+++ client/propftx/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { MovieCard } from './components/MovieCard'
 import { Navbar } from './components/NavBar'
@@ -11,7 +9,7 @@ function App() {
 
   return (
     <>
-    <Navbar/>
+      <Navbar/>
      <MovieCard/>
     </>
   )
